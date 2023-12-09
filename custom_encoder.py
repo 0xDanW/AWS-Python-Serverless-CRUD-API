c0b3be269b433522523436902a8409b0ad4fc82d
Daniel Wong <wongyuheng2003@gmail.com>
2023-12-10 01:20:44 +0800

    Edited lambda file

--- custom_encoder.py
+++ custom_encoder.py
@@ -2,6 +2,7 @@ import json
 from decimal import Decimal
 
 
+### Custom JSON encoder class that takes in and checks for decimal instances and convert into float
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,18 @@
 import boto3
 import json
-from custom_encoder import CustomEncoder
 import logging
+from custom_encoder import CustomEncoder
 
+### Initialize Python logging module
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+### Initialize AWS DynamoDB resource and table
 dynamoTableName = "product-inventory"
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(dynamoTableName)
 
+### Define HTTP methods and API endpoint variables
 getMethod = "GET"
 postMethod = "POST"
 patchMethod = "PATCH"
@@ -19,6 +22,7 @@ productPath = "/product"
 productsPath = "/products"
 
 
+### Main Lambda function
 def lambda_handler(event, context):
     logger.info(event)
     httpMethod = event["httpMethod"]
@@ -26,7 +30,7 @@ def lambda_handler(event, context):
     if httpMethod == getMethod and path == healthPath:
         response = buildResponse(200)
     elif httpMethod == getMethod and path == productPath:
-        response = getProduct(event["queryStringParamters"]["productId"])
+        response = getProduct(event["queryStringParameters"]["productid"])
     elif httpMethod == getMethod and path == productsPath:
         response = getProducts()
     elif httpMethod == postMethod and path == productPath:
@@ -46,6 +50,7 @@ def lambda_handler(event, context):
     return response
 
 
+### API response builder function
 def buildResponse(statusCode, body=None):
     response = {
         "statusCode": statusCode,
@@ -57,3 +62,77 @@ def buildResponse(statusCode, body=None):
     if body is not None:
         response["body"] = json.dumps(body, cls=CustomEncoder)
     return response
+
+
+### Fetches a specific product
+def getProduct(productId):
+    try:
+        response = table.get_item(Key={"productId": productId})
+        if "Item" in response:
+            return buildResponse(200, response["Item"])
+        else:
+            return buildResponse(
+                404, {"Message": "ProductId: %s not found" % productId}
+            )
+    except Exception as e:
+        logger.exception("An error occurred: %s", str(e))
+
+
+# Fetches entire table of products
+def getProducts():
+    try:
+        response = table.scan()
+        result = response["Items"]
+
+        while "LastEvaluatedKey" in response:
+            response = table.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
+            result.extend(response["Items"])
+        body = {"products": result}
+        return buildResponse(200, body)
+    except Exception as e:
+        logger.exception("An error occurred: %s", str(e))
+
+
+# Creates new product object into table
+def saveProduct(requestBody):
+    try:
+        table.put_item(item=requestBody)
+        body = {"Operation": "SAVE", "Message": "SUCCESS", "Item": requestBody}
+        return buildResponse(200, body)
+    except Exception as e:
+        logger.exception("An error occurred: %s", str(e))
+
+
+# Modifies existing product
+def modifyProduct(productId, updateKey, updateValue):
+    try:
+        response = table.update_item(
+            Key={"productId": productId},
+            UpdateExpression="set %s = :value" % updateKey,
+            ExpressionAttributeValues={":value": updateValue},
+            ReturnValue="UPDATED_NEW",
+        )
+        body = {
+            "Operation": "UPDATE",
+            "Message": "SUCCESS",
+            "UpdatedAttributes": response,
+        }
+        return buildResponse(200, body)
+    except Exception as e:
+        logger.exception("An error occurred: %s", str(e))
+
+
+# Deletes exisiting product
+def deleteProduct(productId):
+    try:
+        response = table.delete_item(
+            Key={"productId": productId}, ReturnValues="ALL_OLD"
+        )
+        body = {
+            "Operation": "DELETE",
+            "Message": "SUCCESS",
+            "UpdatedAttributes": response,
+        }
+        return buildResponse(200, body)
+    except Exception as e:
+        logger.exception("An error occurred: %s", str(e))
